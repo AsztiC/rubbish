db8667be6222d6b050a89caf2313432a40fefd9a
AsztiC <aszti.c3@gmail.com>
2025-01-26 00:06:22 -0500

    trained model

--- rubbish-backend/yolov_11_custom/train.py
+++ rubbish-backend/yolov_11_custom/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11l.pt")
 
-model.train(data = "dataset_custom.yaml", imgsz = 640, batch = 8, epochs = 100, workers = 0, device = "0")
+model.train(data = "dataset_custom.yaml", imgsz = 640, batch = -1, epochs = 200, workers = 0, device = "0")
