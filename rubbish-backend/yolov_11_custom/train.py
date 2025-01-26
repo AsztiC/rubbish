a4f4f480e70ff31ae54e79a39e5d0e0de19ad23b
AsztiC <aszti.c3@gmail.com>
2025-01-25 21:28:25 -0500

    yolo train
    train model on gpu

--- rubbish-backend/yolov_11_custom/train.py
+++ rubbish-backend/yolov_11_custom/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11l.pt")
 
-model.train(data = "dataset_custom.yaml", imgsz = 640, batch = 8, epochs = 100, workers = 1, device = "cpu")
+model.train(data = "dataset_custom.yaml", imgsz = 640, batch = 8, epochs = 100, workers = 0, device = "0")
